c2deab3c7338451a4855f49ae194589ac2d2afb6
sotoezequiel <ezequiel_soto_f4@hotmail.com>
2024-12-30 21:53:11 -0300

    se borro la carpeta src

--- src/component/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import image from "../assets/logo.jpeg";
-import styles from "./Logo.module.css";
-
-const Logo = () => {
-  return <img src={image} alt="Logo" className={styles.logo} />;
-};
-
-export default Logo;
